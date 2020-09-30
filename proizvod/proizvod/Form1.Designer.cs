e61866ee74c2c443b28e32460e1a5b5249c3abd0
nena03 <ndjokic928@gmail.com>
2020-09-30 12:16:30 +0200

    update:30.09.2020.

---
+++ proizvod/proizvod/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace proizvod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbProizvod = new System.Windows.Forms.TextBox();
+            this.tbNovaCena = new System.Windows.Forms.TextBox();
+            this.tbNaziv2 = new System.Windows.Forms.TextBox();
+            this.tbProizvodjac2 = new System.Windows.Forms.TextBox();
+            this.tbCena2 = new System.Windows.Forms.TextBox();
+            this.tbPoruka = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbProizvod
+            // 
+            this.tbProizvod.Location = new System.Drawing.Point(12, 53);
+            this.tbProizvod.Name = "tbProizvod";
+            this.tbProizvod.Size = new System.Drawing.Size(319, 20);
+            this.tbProizvod.TabIndex = 0;
+            // 
+            // tbNovaCena
+            // 
+            this.tbNovaCena.Location = new System.Drawing.Point(21, 113);
+            this.tbNovaCena.Name = "tbNovaCena";
+            this.tbNovaCena.Size = new System.Drawing.Size(100, 20);
+            this.tbNovaCena.TabIndex = 1;
+            // 
+            // tbNaziv2
+            // 
+            this.tbNaziv2.Location = new System.Drawing.Point(21, 180);
+            this.tbNaziv2.Name = "tbNaziv2";
+            this.tbNaziv2.Size = new System.Drawing.Size(100, 20);
+            this.tbNaziv2.TabIndex = 2;
+            // 
+            // tbProizvodjac2
+            // 
+            this.tbProizvodjac2.Location = new System.Drawing.Point(21, 207);
+            this.tbProizvodjac2.Name = "tbProizvodjac2";
+            this.tbProizvodjac2.Size = new System.Drawing.Size(100, 20);
+            this.tbProizvodjac2.TabIndex = 3;
+            // 
+            // tbCena2
+            // 
+            this.tbCena2.Location = new System.Drawing.Point(21, 233);
+            this.tbCena2.Name = "tbCena2";
+            this.tbCena2.Size = new System.Drawing.Size(100, 20);
+            this.tbCena2.TabIndex = 4;
+            // 
+            // tbPoruka
+            // 
+            this.tbPoruka.Location = new System.Drawing.Point(21, 296);
+            this.tbPoruka.Name = "tbPoruka";
+            this.tbPoruka.Size = new System.Drawing.Size(100, 20);
+            this.tbPoruka.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Promeni";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 53);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Koji je skuplji prozvod";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbPoruka);
+            this.Controls.Add(this.tbCena2);
+            this.Controls.Add(this.tbProizvodjac2);
+            this.Controls.Add(this.tbNaziv2);
+            this.Controls.Add(this.tbNovaCena);
+            this.Controls.Add(this.tbProizvod);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbProizvod;
+        private System.Windows.Forms.TextBox tbNovaCena;
+        private System.Windows.Forms.TextBox tbNaziv2;
+        private System.Windows.Forms.TextBox tbProizvodjac2;
+        private System.Windows.Forms.TextBox tbCena2;
+        private System.Windows.Forms.TextBox tbPoruka;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ proizvod/proizvod/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proizvod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Proizvod p;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Text = "Test aplikacija";
+
+                p = new Proizvod("Kifla", "Pekara Ljupce");
+                tbProizvod.Text = p.Prikaz();
+                tbProizvod.ReadOnly = true;
+            }
+            catch (Exception izuzetak)
+            {
+                MessageBox.Show(izuzetak.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double novaCena = Convert.ToDouble(tbNovaCena.Text);
+                p.PromeniCenu(novaCena);
+                tbProizvod.Text = p.Prikaz();
+            }
+            catch (Exception izuzetak)
+            {
+                MessageBox.Show(izuzetak.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string naziv = tbNaziv2.Text;
+                string proizvodjac = tbProizvodjac2.Text;
+                double cena = Convert.ToDouble(tbCena2.Text);
+                Proizvod p2 = new Proizvod(naziv, proizvodjac, cena);
+
+                if (p.SkupljiOd(p2))
+                    tbPoruka.Text = "Skuplji je prvi";
+                else if (p2.SkupljiOd(p))
+                    tbPoruka.Text = "Skuplji je drugi";
+                else tbPoruka.Text = "Kostaju isto";
+            }
+            catch (Exception izuzetak)
+            {
+                MessageBox.Show(izuzetak.Message);
+            }
+        }
+    }
+}
